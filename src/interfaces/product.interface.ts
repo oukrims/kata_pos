d19b51b21d95ce536a673595dd6a672146cfb0f2
Salah <oukrimsalah@gmail.com>
2025-06-26 07:28:45 +0100

    feat: add product interface contract

---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,11 @@
+import { Product } from '../core/types';
+
+export interface IProductService {
+  addProduct(product: Omit<Product, 'id'>): Product;
+  getProduct(id: string): Product | null;
+  getAllProducts(): Product[];
+  updateProduct(id: string, updates: Partial<Product>): Product | null;
+  deleteProduct(id: string): boolean;
+  findProductsByName(name: string): Product[];
+  findProductsByCategory(category: string): Product[];
+}
