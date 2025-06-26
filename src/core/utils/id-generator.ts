74c29251f58db29880315de81c1cb9fce0644375
Salah <oukrimsalah@gmail.com>
2025-06-26 06:22:43 +0100

    feat: implement basic generateId function to pass tests

--- src/core/utils/id-generator.ts
+++ src/core/utils/id-generator.ts
@@ -1,4 +1,10 @@
 export function generateId() : string {
-  return ""
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < 8; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
 }
 
--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,12 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import {generateId} from '../core/utils/id-generator'
+
 describe("utils test", () => {
   describe("random id generator",() => {
-    it("should fail to create a random id", ()=> {
+    it("should create a random id", ()=> {
       const id = generateId()
       expect(id).toBeDefined()
       expect(typeof id).toBe('string')
-      expect(id.length).toBeLessThan(1)
+      expect(id.length).toBeGreaterThan(7)
     })
   })
 })
